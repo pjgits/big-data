a08b7d178cbc86caf02fedc0cefeb10775aeb52f
hzpengjie <hzpengjie@corp.netease.com>
2017-03-05 15:11:25 +0800

    change hostname

--- common/src/main/java/com/pj/data/common/Env.java
+++ common/src/main/java/com/pj/data/common/Env.java
@@ -4,6 +4,6 @@ package com.pj.data.common;
  * Created by pengj on 2017-03-04.
  */
 public class Env {
-    public static final String HDFS_URI="hdfs://hadoop-nn-01:9000%s";
+    public static final String HDFS_URI="hdfs://hadoop-nn-1:9000%s";
 
 }
